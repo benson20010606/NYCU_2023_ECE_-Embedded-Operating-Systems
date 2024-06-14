c0ba08320d46ff7c7be3c7f950857c884e0e2440
benson chen <benson20010606@gmail.com>
2024-06-14 20:13:36 +0800

    update lab6

---
+++ Lab/312512032_eos_lab6/client.c
@@ -0,0 +1,105 @@
+#include <fcntl.h>     // open()
+#include <netinet/in.h> // struct sockaddr_in
+#include <arpa/inet.h>  // htons()
+#include <errno.h>
+#include <stdio.h>      // fprintf(), perror()
+#include <stdlib.h>     // exit()
+#include <string.h>     // memset()
+#include <stdint.h>
+
+#include <sys/socket.h> // socket
+#include <signal.h>// signal() 
+#include <unistd.h>    // read(), write(), close() ，dup2()
+#include <sys/sem.h>
+
+#define my_key 11223344
+#define buffersize 128
+
+int sem;
+
+int P(int s){
+    struct sembuf sop;
+    
+    sop.sem_num=0; //binary
+    sop.sem_op=-1; 
+    sop.sem_flg=0;
+    if (semop(s,&sop,1)<0){
+        fprintf(stderr,"P(): semop failed: %s\n",strerror(errno));
+        return -1;
+    } else {
+        return 0;   
+
+    }
+}
+int V(int s){
+    struct sembuf sop;
+    
+    sop.sem_num=0; //binary
+    sop.sem_op=1; 
+    sop.sem_flg=0;
+    if (semop(s,&sop,1)<0){
+        fprintf(stderr,"V(): semop failed: %s\n",strerror(errno));
+        return -1;
+    } else {
+        return 0;   
+
+    }
+}
+
+
+int main(int argc, char *argv[]) {
+    if(argc != 6) {
+        fprintf(stderr, "Usage: ./client <server_ip> <port> <deposit/withdraw> <amount> <times>");
+        exit(EXIT_FAILURE);
+    }
+    int amount=atoi(argv[4]);
+    int times=atoi(argv[5]);
+    char send_buf[buffersize]={0};
+    sem=semget(my_key,1,0);
+    
+    if (sem < 0) {
+    fprintf (stderr," cannot find semaphore %d: %s\n", my_key, strerror(errno));
+    exit(1);
+    }
+
+    int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if ( socket_fd < 0) {
+        printf("Create socket fail!\n");
+        exit(EXIT_FAILURE);
+    }
+    //printf("socket has been created \n");
+ 
+    struct sockaddr_in cli_Addr; 
+    bzero(&cli_Addr,sizeof(cli_Addr));
+    cli_Addr.sin_family = PF_INET;
+    cli_Addr.sin_addr.s_addr = inet_addr(argv[1]);
+    cli_Addr.sin_port = htons(atoi(argv[2]));
+    if (connect(socket_fd, (struct sockaddr *)&cli_Addr, sizeof(cli_Addr)) == -1) {
+        printf("Connect server failed!\n");
+        close(socket_fd);
+        exit(0);
+    }
+    //printf("Connect server [%s:%d] success\n",inet_ntoa(cli_Addr.sin_addr), ntohs(cli_Addr.sin_port));
+   
+
+
+    for(int count=0;count<times;count++){
+        P(sem);
+        sprintf(send_buf,"%s %d",argv[3],amount);
+        //printf("%d\n",count);
+        send(socket_fd, send_buf, buffersize, 0);
+        memset(send_buf,0,buffersize);
+        V(sem);
+        usleep(1);
+      
+    }
+    sprintf(send_buf,"exit");
+    send(socket_fd, send_buf, buffersize, 0);
+    memset(send_buf,0,buffersize);
+    
+    close(socket_fd);
+    
+    return 0;
+}
+
+
---
+++ Lab/312512032_eos_lab6/server.c
@@ -0,0 +1,138 @@
+#include <fcntl.h>     // open()
+#include <netinet/in.h> // struct sockaddr_in
+#include <arpa/inet.h>  // htons()
+#include <errno.h>
+#include <stdio.h>      // fprintf(), perror()
+#include <stdlib.h>     // exit()
+#include <string.h>     // memset()
+#include <stdint.h>
+#include <sys/socket.h> // socket
+#include <signal.h>// signal() 
+#include <unistd.h>    // read(), write(), close() ，dup2()
+
+#include <sys/sem.h>
+#include <pthread.h>
+
+#define buffersize 128
+#define my_key 11223344
+#define SEM_MODE 0666
+int  server_socket;
+int  client_socket;
+int s;
+int my_money_now=0;
+//pthread_mutex_t mutex;
+//int a=1;
+//int b=1;
+void sigint_handler(int signo) {
+    close(server_socket);
+    //pthread_mutex_destroy(&mutex);
+    if(semctl(s,0,IPC_RMID,0)<0){
+        fprintf (stderr, " unable to remove semaphore %d\n", my_key);
+        exit(1);  
+    }
+    printf("\nSemaphore %d has been remove\n", my_key);
+    printf("socket  has been close\n");
+    exit(0);
+}
+
+
+void* bank(void* socket){   
+        char receive[buffersize]={0};
+        char what_to_do[10]={0}; 
+        int sock=*(int*)socket;
+        while((recv(sock,receive,buffersize,0))){
+            //pthread_mutex_lock(&mutex);
+            int money=0;
+            //printf("%s\n",receive);
+            sscanf(receive,"%s %d",what_to_do,&money);
+            //printf("%d\n",money);
+            if(strcmp(what_to_do,"deposit")==0){
+                my_money_now=my_money_now+money;
+                printf("After deposit(%d):%d\n",money,my_money_now);//a++;
+            }
+            else if(strcmp(what_to_do,"withdraw")==0){
+                my_money_now=my_money_now-money;
+                printf("After withdraw(%d):%d\n",money, my_money_now);//b++;
+            }else if(strcmp(what_to_do,"exit")==0){
+            	break;
+            }
+            
+            //pthread_mutex_unlock(&mutex);
+        }
+        pthread_exit(NULL);
+}
+
+
+
+int main(int argc, char *argv[]) {
+
+    //pthread_mutex_init(&mutex,NULL);
+    signal(SIGINT, sigint_handler);
+   
+
+    if(argc != 2) {
+        fprintf(stderr, "Usage: ./server  <port>");
+        exit(EXIT_FAILURE);
+    }
+
+
+    s=semget(my_key,1,IPC_CREAT | IPC_EXCL | SEM_MODE);
+    if(s<0){
+        fprintf(stderr,"cannot find semaohore %d :%s\n",my_key,strerror(errno));
+        exit(1);
+    }
+    if ( semctl(s, 0, SETVAL, 1) < 0 )
+    {
+        fprintf(stderr, "Unable to initialize Sem: %s\n", strerror(errno));
+        exit(0);
+    }
+    //printf("Semaphore %d has been created & initialized to 1\n", my_key);
+
+
+    if ((server_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("socket()");
+        semctl(s,0,IPC_RMID,0);
+        exit(EXIT_FAILURE);
+    }
+    //printf("server_socket has been created \n");
+
+    int yes = 1;
+    setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes));
+    
+    struct sockaddr_in serverAddr={
+    .sin_family=PF_INET,
+    //.sin_addr.s_addr=inet_addr("127.0.0.1"),
+    .sin_addr.s_addr=INADDR_ANY,
+    .sin_port =htons(atoi(argv[1]))
+    };
+ 
+    if(bind(server_socket,(const struct sockaddr *)&serverAddr,sizeof(serverAddr))<0){
+        perror("bind_error");
+        semctl(s,0,IPC_RMID,0);
+        close(server_socket);
+        exit(EXIT_FAILURE);
+    }
+    //printf("bind finished \n");
+    if (listen(server_socket, 10) < 0) {
+        perror("listen");
+        semctl(s,0,IPC_RMID,0);
+        close(server_socket);
+        exit(EXIT_FAILURE);
+    }
+    //printf("listen finished \n");
+   
+    pthread_t thread_id[100];
+    int i=0;
+    while (1) {
+        struct sockaddr_in client_addr;
+        socklen_t client_addr_len = sizeof(client_addr);
+        client_socket = accept(server_socket, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (client_socket < 0) {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        pthread_create(&thread_id[i],NULL,bank,(void*)&client_socket);
+        i++;
+    }
+return 0;
+}
